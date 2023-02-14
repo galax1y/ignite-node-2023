9245ebd8e1a7bac4e7e6ce8f4bb253dea1c21643
Lucas Wermann <lw1707@hotmail.com>
2023-02-14 14:30:53 -0300

    Stream e Buffer p/ receber informações JSON no corpo da requisição

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,9 +1,22 @@
+import { randomUUID } from 'node:crypto'
 import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
 	const {method, url} = request
+
+	const buffers = []
+
+	for await (const chunk of request) {
+		buffers.push(chunk)
+	}
+
+	try {
+		request.body = JSON.parse(Buffer.concat(buffers).toString())
+	} catch (error) {
+		console.log(error)
+	}
 
 	if (method === 'GET' && url === '/users') {
 		return response
@@ -12,10 +25,11 @@ const server = http.createServer((request, response) => {
 	}
 
 	if (method === 'POST' && url === '/users') {
+		const { name, email } = request.body
 		users.push({
-			id: 1,
-			name: 'Fulano',
-			email: 'fulano@example.com',
+			id: randomUUID(),
+			name,
+			email,
 		})
 		return response.writeHead(201).end('Criação de usuário')
 	}
