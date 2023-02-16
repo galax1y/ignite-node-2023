67c09b8a5a69645bf945364f6091bba75ff8d10e
Lucas Wermann <lw1707@hotmail.com>
2023-02-16 14:02:35 -0300

    Configurando ESLint

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -3,13 +3,13 @@ import fastify from 'fastify'
 const app = fastify()
 
 app.get('/hello', () => {
-	return 'hey'
+  return 'hey'
 })
 
 app
-	.listen({
-		port: 3333,
-	})
-	.then(() => {
-		console.log('HTTP Server running on port 3333')
-	})
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server running on port 3333')
+  })
