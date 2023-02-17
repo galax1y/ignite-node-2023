a1d25c3ba6b2882d8e8aee038ab69537d7e89097
Lucas Wermann <lw1707@hotmail.com>
2023-02-17 20:56:14 -0300

    Criando testes automatizados que faltavam

--- 02-api-rest-nodejs/test/transactions.spec.ts
+++ 02-api-rest-nodejs/test/transactions.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test, beforeAll, afterAll, describe, beforeEach } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
-import { exec, execSync } from 'child_process'
+import { execSync } from 'child_process'
 
 // test('enunciado', () => {
 //  operação
@@ -57,6 +57,62 @@ describe('/transactions route tests', () => {
           amount: 5000,
         }),
       ]),
+    )
+  })
+
+  test('it should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+    expect(listTransactionsResponse.statusCode).toEqual(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionFromId = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+
+    expect(getTransactionFromId.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  test('it should be able to get the summary', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const transactionsSummary = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+
+    expect(transactionsSummary.statusCode).toEqual(200)
+
+    expect(transactionsSummary.body).toEqual(
+      expect.objectContaining({
+        summary: expect.objectContaining({
+          amount: expect.any(Number),
+        }),
+      }),
     )
   })
 })
