bf4bb7434285ef3e03f65f61620eb4809a358e09
Lucas Wermann <lw1707@hotmail.com>
2023-02-17 16:41:25 -0300

    Modificando estrutura p/ testes + Primeiro teste

--- 02-api-rest-nodejs/test/example.spec.ts
+++ 02-api-rest-nodejs/test/example.spec.ts
@@ -1,7 +1,26 @@
-import { test } from 'vitest'
+import { expect, test, beforeAll, afterAll } from 'vitest'
+import { app } from '../src/app'
+import request from 'supertest'
+
 // test('enunciado', () => {
 //  operação
 //  validação - expect()
 // })
 
-test('user should be able to create a new transaction', () => {})
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('user should be able to create a new transaction', async () => {
+  const response = await request(app.server).post('/transactions').send({
+    title: 'New transaction',
+    amount: 5000,
+    type: 'credit',
+  })
+
+  expect(response.statusCode).toEqual(201)
+})
