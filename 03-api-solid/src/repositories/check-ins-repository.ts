0fe32b8f6ef1c5f7ba0a15645050fb4e37d44c51
LucasWermann <lucasvw.eng@gmail.com>
2023-03-16 17:04:40 -0300

    Add: useCase Listar histórico CheckIns + Paginação

--- 03-api-solid/src/repositories/check-ins-repository.ts
+++ 03-api-solid/src/repositories/check-ins-repository.ts
@@ -3,5 +3,6 @@ import { CheckIn, Prisma } from '@prisma/client'
 export interface CheckInsRepository {
   // UncheckedCreateInput do Prisma considera que os objetos Gym e User já estão criados
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
