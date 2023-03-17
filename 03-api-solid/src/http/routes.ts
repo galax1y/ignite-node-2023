1f1e7c44cf1c4fd54cbbc01a1b09d9e70cc8fbb7
LucasWermann <lucasvw.eng@gmail.com>
2023-03-17 15:50:25 -0300

    Add: getUserProfile useCase + Separar JWT auth em um Middleware

--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   /* Rotas para usuários ainda não autenticados */
@@ -9,5 +10,6 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
 
   /* Rotas para usuários autenticados */
-  app.get('/me', profile)
+  // { onRequest: [verifyJWT] } é um middleware para autenticação, caso não esteja autenticado retornará 401 Unauthorized
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
