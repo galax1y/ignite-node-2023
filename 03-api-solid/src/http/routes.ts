96f8c357c10ec2386abee385d910a13ffc211ea2
LucasWermann <lucasvw.eng@gmail.com>
2023-03-17 15:38:29 -0300

    Add: Rota autenticada com JWT

--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 import { register } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
+  /* Rotas para usuários ainda não autenticados */
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /* Rotas para usuários autenticados */
+  app.get('/me', profile)
 }
