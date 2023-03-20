7a3c8964e1b5ce98c8041090d3f51f6dbf560419
LucasWermann <lucasvw.eng@gmail.com>
2023-03-20 14:29:30 -0300

    Add: Testes controllers de Users + controllers Gym

--- 03-api-solid/src/http/controllers/users/routes.ts
+++ 03-api-solid/src/http/controllers/users/routes.ts
@@ -1,10 +1,10 @@
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
-import { authenticate } from './controllers/authenticate'
-import { profile } from './controllers/profile'
-import { register } from './controllers/register'
-import { verifyJWT } from './middlewares/verify-jwt'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { register } from './register'
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   /* Rotas para usuários ainda não autenticados */
   app.post('/users', register)
   app.post('/sessions', authenticate)
