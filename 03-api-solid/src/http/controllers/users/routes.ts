63e07890b1947d13c9b5e8b8824f042b17f12abd
LucasWermann <lucasvw.eng@gmail.com>
2023-03-21 15:29:27 -0300

    Add: Rota p/ validar e renovar o Refresh Token

--- 03-api-solid/src/http/controllers/users/routes.ts
+++ 03-api-solid/src/http/controllers/users/routes.ts
@@ -2,14 +2,14 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 import { register } from './register'
 
 export async function usersRoutes(app: FastifyInstance) {
-  /* Rotas para usuários ainda não autenticados */
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
-  /* Rotas para usuários autenticados */
-  // { onRequest: [verifyJWT] } é um middleware para autenticação, caso não esteja autenticado retornará 401 Unauthorized
+  app.patch('/token/refresh', refresh)
+
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
