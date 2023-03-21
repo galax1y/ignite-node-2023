07efaf987d6d1fc290c6a7346a94b439325e6381
LucasWermann <lucasvw.eng@gmail.com>
2023-03-21 16:05:13 -0300

    Add: Implement. Role Based Authorization Control

--- 03-api-solid/src/@types/fastify-jwt.d.ts
+++ 03-api-solid/src/@types/fastify-jwt.d.ts
@@ -2,9 +2,9 @@ import '@fastify/jwt'
 
 declare module '@fastify/jwt' {
   export interface FastifyJWT {
-    payload: { id: number }
     user: {
       sub: string
+      role: 'ADMIN' | 'MEMBER'
     }
   }
 }
