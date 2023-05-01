6bab34ab9435a8718725c18158c6541c4fae78a4
LucasWermann <lucasvw.eng@gmail.com>
2023-04-30 23:08:39 -0300

    Add: Resto do protótipo das rotas

--- 02-desafio/src/routes/meals.ts
+++ 02-desafio/src/routes/meals.ts
@@ -4,6 +4,10 @@ import { knex } from "../database";
 import { z } from "zod";
 import NewDateAsIso from "../utils/get-new-date-as-iso";
 
+const updateMealRequestParamsSchema = z.object({
+  mealId: z.string().uuid()
+})
+
 export async function mealsRoutes(app: FastifyInstance) {
   // Todas rotas nesse contexto devem passar por esse hook preHandler
   app.addHook('preHandler', checkCookiesForUserId)
@@ -17,6 +21,55 @@ export async function mealsRoutes(app: FastifyInstance) {
     reply.status(200).send(meals)
   })
 
+  app.delete('/:mealId', async (request, reply) => {
+    console.log('Rota DELETE foi acionada')
+
+    const deleteMealRequestParamsSchema = z.object({
+      mealId: z.string().uuid()
+    })
+
+    try {
+      const { mealId } = deleteMealRequestParamsSchema.parse(request.params)
+
+      await knex('meals').delete('*').where({
+        meal_id: mealId,
+        user_id: request.cookies.userId
+      })
+
+      reply.status(200).send()
+    } catch (error) {
+      reply.status(400).send({message: error})
+    }
+  })
+
+  // Deve ser possível editar uma refeição
+  app.patch('/:mealId', async (request, reply) => {
+    console.log('Rota UPDATE foi acionada')
+
+    const updateMealRequestBodySchema = z.object({
+      name: z.string().min(3),
+      description: z.string().optional(),
+      is_healthy: z.boolean(),
+    })
+
+    try {
+      const { mealId } = updateMealRequestParamsSchema.parse(request.params)
+      // Validar se essa refeição existe
+
+      const data = updateMealRequestBodySchema.parse(request.body)
+    
+      await knex('meals').update({
+        ...data
+      }).where({
+        meal_id: mealId,
+        user_id: request.cookies.userId
+      })
+
+      reply.status(200).send()
+    } catch (error) {
+      reply.status(400).send({message: error})
+    }
+  })
 
 
   // Deve ser possível registrar uma refeição feita
@@ -48,7 +101,21 @@ export async function mealsRoutes(app: FastifyInstance) {
       reply.status(201).send()
     }
     catch (error) {
-      reply.status(400).send(error)
+      reply.status(400).send({message: error})
+    }
+  })
+
+  app.get('/:mealId', async (request, reply) => {
+    try {
+      const { mealId } = updateMealRequestParamsSchema.parse(request.params)
+
+      const meal = await knex('meals').select('*').where({
+        meal_id: mealId
+      })
+      
+      reply.status(200).send(meal)
+    } catch (error) {
+      reply.status(400).send({message: error})
     }
   })
 }
--- 02-desafio/src/routes/users.ts
+++ 02-desafio/src/routes/users.ts
@@ -21,4 +21,18 @@ export async function usersRoutes(app: FastifyInstance) {
     
     reply.status(201).send({user})
   })
+
+  app.get('/metrics', async (request, reply) => {
+    const userMeals = await knex('meals').where({
+      user_id: request.cookies.userId
+    })
+
+    const metrics = {
+      totalMeals: userMeals.length,
+      onDietMeals: userMeals.filter((meal) => meal.is_healthy).length,
+      offDietMeals: userMeals.filter((meal) => !meal.is_healthy).length
+    }
+
+    reply.status(200).send(metrics)
+  })
 }
