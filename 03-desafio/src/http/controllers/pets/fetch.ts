56cd0b3a665067e3dab32defabc60d4bdc3c00d5
LucasWermann <lucasvw.eng@gmail.com>
2023-05-04 18:37:05 -0300

    Add: Rota p/ procurar pet específico

--- 03-desafio/src/http/controllers/pets/fetch.ts
+++ 03-desafio/src/http/controllers/pets/fetch.ts
@@ -1,1 +1,26 @@
 // Fetch unique pet
+import { makeFetchPetDetailsUseCase } from '@/use-cases/factories/make-fetch-pet-details-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function fetch(request: FastifyRequest, reply: FastifyReply) {
+  const fetchPetDetailsParamsSchema = z.object({
+    petId: z.string().uuid(),
+  })
+
+  try {
+    const { petId } = fetchPetDetailsParamsSchema.parse(request.params)
+
+    const fetchPetDetailsUseCase = makeFetchPetDetailsUseCase()
+
+    const pet = await fetchPetDetailsUseCase.execute({ petId })
+
+    if (!pet) {
+      reply.status(404).send({ message: 'Pet not found' })
+    }
+
+    reply.status(200).send(pet)
+  } catch (err) {
+    reply.send({ message: `This route needs a 'petId' query parameter` })
+  }
+}
--- 03-desafio/src/http/controllers/pets/routes.ts
+++ 03-desafio/src/http/controllers/pets/routes.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { search } from './search'
+import { fetch } from './fetch'
 import { verifyAuth } from '@/http/middlewares/verify-auth'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.get('/pets', search)
+  app.get('/pets/:petId', fetch)
 
   /* Rotas autenticadas */
   app.post('/pets', { onRequest: [verifyAuth] }, register)
