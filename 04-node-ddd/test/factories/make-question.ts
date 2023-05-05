2f078601b99507db71e189d04cfa0b24a9a42dae
LucasWermann <lucasvw.eng@gmail.com>
2023-05-05 16:31:32 -0300

    Add: Lib Fakerjs p/ gerar dados fictícios

--- 04-node-ddd/test/factories/make-question.ts
+++ 04-node-ddd/test/factories/make-question.ts
@@ -1,21 +1,26 @@
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { faker } from '@faker-js/faker'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 
 // Partial torna todos os campos opcionais
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    // Propriedades padrões
-    authorId: new UniqueEntityID('1'),
-    title: 'Example question title',
-    slug: Slug.create('example-question-title'),
-    content: 'Example question content',
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      // Propriedades padrões
+      authorId: new UniqueEntityID('1'),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
 
-    ...override, // Se houver algo no override, ela sobrescreve as propriedades padrões
-  })
+      ...override, // Se houver algo no override, ela sobrescreve as propriedades padrões
+    },
+    id,
+  )
 
   return question
 }
